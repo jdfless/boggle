385df6411e4fa7c708c47003b5f2db42827743bc
Jonathan Flessner <jdf@email.com>
2017-03-07 20:31:34 -0800

    fix typo in touches key 10, had 8 instead of 9, shuffle board on each run

--- boggle.py
+++ boggle.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 
 from copy import deepcopy
-from random import randint
+from random import randint, shuffle
 import sys
 
 TOUCHES = { 0: [1, 4, 5], 1: [0, 2, 4, 5, 6], 2: [1, 3, 5, 6, 7],
             3: [2, 6, 7], 4: [0, 1, 5, 8, 9], 5: [0, 1, 2, 4, 6, 8, 9, 10], 
             6: [1, 2, 3, 5, 7, 9, 10, 11], 7: [2, 3, 6, 10, 11],
             8: [4, 5, 9, 12, 13], 9: [4, 5, 6, 8, 10, 12, 13, 14],
-            10: [5, 6, 7, 8, 11, 13, 14, 15], 11: [6, 7, 10, 14, 15],
+            10: [5, 6, 7, 9, 11, 13, 14, 15], 11: [6, 7, 10, 14, 15],
             12: [8, 9, 13], 13: [8, 9, 10, 12, 14], 14: [9, 10, 11, 13, 15],
             15: [10, 11, 14] 
            }
@@ -40,6 +40,7 @@ def roll_cubes():
     board = []
     for cube in cubes:
         board.append(cube[roll()])
+        shuffle(board)
     
     return board
 
